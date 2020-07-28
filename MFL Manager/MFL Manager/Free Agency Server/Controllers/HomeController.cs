cbe3898aad89db639533e7f2a7d2b754a9fd35f3
RobbieOstermann <robbie.ostermann@cerner.com>
2020-07-28 14:26:15 -0500

    Beginning to implement SignalR.

--- MFL Manager/MFL Manager/Free Agency Server/Controllers/HomeController.cs
+++ MFL Manager/MFL Manager/Free Agency Server/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Free_Agency_Server.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Franchise Information";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Information";
 
             return View();
         }
