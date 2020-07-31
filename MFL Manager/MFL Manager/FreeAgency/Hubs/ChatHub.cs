f8e9fc44ab1338743938fba8db965233d481192b
RobertOstermann <loothunder@gmail.com>
2020-07-30 20:51:58 -0500

    Began implementing bid ability.

--- MFL Manager/MFL Manager/FreeAgency/Hubs/ChatHub.cs
+++ MFL Manager/MFL Manager/FreeAgency/Hubs/ChatHub.cs
@@ -8,9 +8,23 @@ namespace FreeAgency.Hubs
 {
     public class ChatHub : Hub
     {
+        //private static Queue<string> messages = new Queue<string>();
+
+        private static double currentBid = 0.00;
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task GetBid()
+        {
+            await Clients.Caller.SendAsync("ReceiveBid", currentBid, 0.00);
+        }
+        public async Task SendBid(double amount)
+        {
+            await Clients.All.SendAsync("ReceiveBid", currentBid, amount);
+            currentBid += amount;
         }
     }
 }
--- MFL Manager/MFL Manager/FreeAgency/wwwroot/js/Chat.js
+++ MFL Manager/MFL Manager/FreeAgency/wwwroot/js/Chat.js
@@ -4,17 +4,25 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
+document.getElementById("bidButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
+    var encodedMsg = user + ": " + msg;
     var li = document.createElement("li");
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
 });
 
+connection.on("ReceiveBid", function (currentBid, amount) {
+    var newBid = parseFloat(currentBid) + parseFloat(amount);
+    document.getElementById("currentBid").innerHTML = newBid.toFixed(2);
+})
+
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
+    document.getElementById("bidButton").disabled = false;
+    document.getElementById("currentBid").innerHTML = connection.invoke("GetBid");
 }).catch(function (err) {
     return console.error(err.toString());
 });
@@ -23,6 +31,14 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("bidButton").addEventListener("click", function (event) {
+    var bidAmount = parseFloat(document.getElementById("bidAmount").value);
+    connection.invoke("SendBid", bidAmount).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
